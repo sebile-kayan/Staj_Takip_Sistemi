1973dddccbb13793dc821df90f4d75ca5e1a24a7
SEBİLE KAYAN <sebilekayan@gmail.com>
2023-09-17 15:13:51 +0300

    stajtakipsistemi

---
+++ crud_staj_takip-main/Staj-Takip-Sistemi-main/crud-main/Staj Takip Sistemi/Staj Takip Sistemi/sqlbaglanti.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staj_Takip_Sistemi
+{
+    public class sqlbaglanti    
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static SqlDataReader dr;
+        public static SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-OSNDT9I\SQLEXPRESS;Initial Catalog=stajtakip;Integrated Security=True");
+    }
+}
